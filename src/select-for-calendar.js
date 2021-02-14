00c7bb2146be21364fee1a3cbea37657fc8b8203
IhorHudkov <gudkov.igor@gmail.com>
2021-02-14 22:49:51 +0300

    Troubleshoot meeting filtering

--- src/select-for-calendar.js
+++ src/select-for-calendar.js
@@ -2,6 +2,7 @@ import './styles/select.scss'
 
 let show = true;
 
+const checkBoxList = document.querySelectorAll('input[type="checkbox"]');
 const selectBox =  document.querySelector('.selectBox');
 selectBox.addEventListener('click', showCheckboxes);
 
@@ -23,9 +24,48 @@ function showCheckboxes() {
 }
 
 window.addEventListener('load', () => {
-    const chbList = document.querySelectorAll('input[type="checkbox"]');
-    for (let item of chbList) {
+    for (let item of checkBoxList) {
         item.checked = true;
     }
 });
+
+
+let checkBoxArray = Array.prototype.slice.call(checkBoxList);
+const allMemCheckBox = checkBoxArray.shift();
+
+console.clear();
+console.log(checkBoxArray);
+console.log(allMemCheckBox);
+
+allMemCheckBox.addEventListener('click', () => {
+    if (allMemCheckBox.checked) {
+        for (let item of checkBoxList) {
+            item.checked = true;
+        }
+    }else{
+        for (let item of checkBoxList) {
+            item.checked = false;
+        }
+    }
+});
+
+
+for (let i of checkBoxArray) {
+    i.addEventListener('click', () => {
+        let count = 0;
+        for (let j of checkBoxArray) {
+            if ( j.checked ) {
+                count++;
+            }
+        }
+        console.log(checkBoxArray.length);
+        if (checkBoxArray.length == count && allMemCheckBox.checked == false) {
+            allMemCheckBox.checked = true;
+        }
+        if (checkBoxArray.length !== count && allMemCheckBox.checked) {
+            allMemCheckBox.checked = false;
+        }
+
+    });
+}
 
--- src/select.js
+++ src/select.js
@@ -1,7 +1,7 @@
 import './styles/select.scss'
 
 let show = true;
-
+const checkBoxList = document.querySelectorAll('input[type="checkbox"]');
 const selectBox =  document.querySelector('.selectBox');
 selectBox.addEventListener('click', showCheckboxes);
 
@@ -17,5 +17,44 @@ function showCheckboxes() {
         checkboxes.style.display = "none";
         show = true;
     }
+}
+
+let checkBoxArray = Array.prototype.slice.call(checkBoxList);
+const allMemCheckBox = checkBoxArray.shift();
+
+console.clear();
+console.log(checkBoxArray);
+console.log(allMemCheckBox);
+
+allMemCheckBox.addEventListener('click', () => {
+    if (allMemCheckBox.checked) {
+        for (let item of checkBoxList) {
+            item.checked = true;
+        }
+    }else{
+        for (let item of checkBoxList) {
+            item.checked = false;
+        }
+    }
+});
+
+
+for (let i of checkBoxArray) {
+    i.addEventListener('click', () => {
+        let count = 0;
+        for (let j of checkBoxArray) {
+            if ( j.checked ) {
+                count++;
+            }
+        }
+        console.log(checkBoxArray.length);
+        if (checkBoxArray.length == count && allMemCheckBox.checked == false) {
+            allMemCheckBox.checked = true;
+        }
+        if (checkBoxArray.length !== count && allMemCheckBox.checked) {
+            allMemCheckBox.checked = false;
+        }
+
+    });
 }
 
